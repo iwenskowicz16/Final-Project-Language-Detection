955c19268cae4e93d2a6f599b27bd61c0fd8f718
Unknown <ianwensk@gmail.com>
2017-12-01 15:12:19 -0500

    Edited partone
    filled in most of the functions

--- partone.h
+++ partone.h
@@ -7,8 +7,8 @@ std::vector<int> makeIntVec();		//creates a vector filled with 0's at the size o
 
 std::vector<std::string> make19kVec();		//creates all possible 3 letter combinations using a triple nested loop
 
-std::vector<int> doProb(std::vector<std::string> gnk, std::vector<int> git, OBJ comparator);
+std::vector<int> doProb(OBJ comparator);
 												//merges the object that keeps track of the file contents with the vector filled with 0's
-OBJ getData(filename);		//gets the data
+//OBJ getData(filename);		//gets the data
 
 #endif
