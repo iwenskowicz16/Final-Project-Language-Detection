938fe30d1806155367d1c3cb16311b10a3fd9804
Unknown <ianwensk@gmail.com>
2017-12-11 12:36:45 -0500

    Attempted to impliment everything other than the compare method
    Restructured the main function to do what we want. Added the toStr method that takes a file name and returns it's contents as 1 string.

--- main.cpp
+++ main.cpp
@@ -2,24 +2,52 @@
 #include <iostream>
 #include <string>
 #include "obj.h"
+#include <fstream>
 
 int main(int argc, char *argv[]){
 	if(argc < 2){exit(EXIT_FAILURE);}			//checks to make sure there is only 1 input
 	for(int i = 1; i <= argc; i++){
 		std::string s = "";
 		s += argv[i];
-		s += ".txt";
+		s += ".txt"
 		ifstream myFile(s);
 		if(myFile.fail()){
 			return 1;
 		}
 	}
-	std::string testString = argv[1];
+	OBJ::OBJ inp = OBJ(toStr(argv[argc - 1]));
+	int big = 0;
+	int pos = 0;
+	for(int i = 1; i < argc; i++){
+		OBJ::OBJ comp = OBJ(toStr(argv[i]));
+		int l = compare(inp.getNum(),comp.getNum());
+		if(big < l){
+			big = l;
+			pos = i;
+		}
+	}
 	OBJ::OBJ test = OBJ(testString);			//runs the functions
 	std::vector<int> freq = test.getNum();		//extracts the result
-	for(int i = 0; i < 19683; i++){
-		std::cout << freq[i] << " ";	
-	}										//prints the result
-	std::cout << std::endl;
+	std::cout << argv[big] << std::endl;		//prints the result
 	return 0;
+}
+
+double compare(std::vector<int> a, std::vector<int> b){
+	double *a, double *b, unsigned int len
+    double idk = 0.0, da = 0.0, db = 0.0 ;
+     for(unsigned int i = 0u; i < len; ++i) {
+        idk += a[i] * b[i] ;
+        da += a[i] * a[i] ;
+        db += b[i] * b[i] ;
+    }
+    return idk / (sqrt(da) * sqrt(db)) ;
+}
+
+std::string toStr(std::string name){
+	ifstream infile;
+	infile.open(name);
+	std::string s;
+	std::getline(infile, s);
+	infile.close();
+	return s;
 }
