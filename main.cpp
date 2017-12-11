7dbd398861de59ca6d29c6ac4065266b7c96f2db
Unknown <ianwensk@gmail.com>
2017-12-11 12:43:38 -0500

    Everything other than compare should be complete
    Everything should be ready for debugging other than compare

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main(int argc, char *argv[]){
 	}
 	OBJ::OBJ inp = OBJ(toStr(argv[argc - 1]));
 	double big = 0;
-	int pos = 0;
-	for(int i = 1; i < argc; i++){
+	int pos = 1;
+	for(int i = 1; i < argc - 1; i++){
 		OBJ::OBJ comp = OBJ(toStr(argv[i]));
 		double l = compare(inp.getNum(),comp.getNum());
 		if(big < l){
@@ -26,19 +26,12 @@ int main(int argc, char *argv[]){
 			pos = i;
 		}
 	}
-	std::cout << argv[big] << std::endl;		//prints the result
+	std::cout << argv[pos] << std::endl;		//prints the result
 	return 0;
 }
 
 double compare(std::vector<int> a, std::vector<int> b){
-	double *a, double *b, unsigned int len
-    double idk = 0.0, da = 0.0, db = 0.0 ;
-     for(unsigned int i = 0u; i < len; ++i) {
-        idk += a[i] * b[i] ;
-        da += a[i] * a[i] ;
-        db += b[i] * b[i] ;
-    }
-    return idk / (sqrt(da) * sqrt(db)) ;
+	
 }
 
 std::string toStr(std::string name){
